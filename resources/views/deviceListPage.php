337d82141add32d6631cf0c1cb0313efa019bf68
dnsl26mw <dnsl26mw@gmail.com>
2026-01-01 22:20:08 +0900

    デバイス一覧ページにテーブルを追加

--- resources/views/deviceListPage.php
+++ resources/views/deviceListPage.php
@@ -1,6 +1,26 @@
 <?php
 // デバイス一覧画面
 
+// デバイス一覧情報
+$deviceListInfo = $data ?? [];
+
 ?>
+
+<table>
+    <tr>
+        <th>選択</th>
+        <th>デバイスID</th>
+        <th>デバイス名</th>
+        <th>MACアドレス</th>
+    </tr>
+    <?php for ($i = 0; $i <= count($deviceListInfo); $i++): ?>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <?php endfor; ?>
+</table>
 
 <a href="/registdevice">デバイスの追加はこちら</a>
