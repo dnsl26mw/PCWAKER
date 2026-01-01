0de696e2a43129ef333248b8a80c1cf44b3e6a65
dnsl26mw <dnsl26mw@gmail.com>
2026-01-01 21:59:19 +0900

    デバイステーブルのテーブルクラス追加

---
+++ database/deviceTable.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once __DIR__ . '/../app/Service/util.php';
+
+class UserTable {
+    // デバイス情報検索
+    public static function searchDeviceInfo(array $data, $db){
+        $stmt = $db->prepare('SELECT device_id, device_name, macaddress FROM devicetable WHERE user_id=?');
+        $stmt->execute(array($data['userID']));
+        $dbRow = $stmt->fetch();
+        return $dbRow;
+    }
+
+    // デバイス情報登録
+    public static function registDeviceInfo(array $data, $db){
+        try{
+            $registStmt = $db->prepare('INSERT INTO usertable(device_id, device_name, macaddress, user_id) VALUES(?, ?, ?, ?)');
+            $registStmt->execute(array($data['deviceID'],  $data['deviceName'], $data['macAddress'], $data['userName']));
+            return true;
+        }
+        catch(Exception $e){
+            return false;
+        }
+    }
+
+    // デバイス情報更新
+    public static function updateDeviceInfo(array $data, $db){
+        try{
+            $registStmt = $db->prepare('UPDATE usertable SET device_name = ?, macaddress = ? WHERE device_id = ?');
+            $registStmt->execute(array($data['deviceName'], $data['macAddress'], $data['deviceID']));
+            return true;
+        }
+        catch(Exception $e){
+            return false;
+        }
+    }
+
+    // デバイス情報削除
+    public static function deleteDeviceInfo(array $data, $db){
+        try{
+            $stmt = $db->prepare('DELETE FROM devicetable WHERE device_id=?');
+            $stmt->execute(array($data['deviceID']));
+            return true;
+        }
+        catch(Exception $e){
+            return false;
+        }
+    }
+
+    // デバイス情報全削除
+    public static function deleteDeviceInfoAll(array $data, $db){
+        try{
+            $stmt = $db->prepare('DELETE FROM devicetable WHERE user_id=?');
+            $stmt->execute(array($data['userID']));
+            return true;
+        }
+        catch(Exception $e){
+            return false;
+        }
+    }
+}
+?>
