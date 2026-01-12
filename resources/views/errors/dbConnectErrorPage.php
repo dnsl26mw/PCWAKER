4d71f70c00c18a8cf0787fc83621e7a09f8ecbbe
dnsl26mw <dnsl26mw@gmail.com>
2026-01-13 00:35:44 +0900

    dbConnectErrorPage.phpをボディのみに変更

--- resources/views/errors/dbConnectErrorPage.php
+++ resources/views/errors/dbConnectErrorPage.php
@@ -1,20 +1,4 @@
-<?php
-// DB接続エラー
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PCWAKER</title>
-    </head>
-    <body>
-        <header>
-            <p>データベースへの接続に失敗しました。システム管理者に連絡してください。</p>
-        </header>
-        <form action="" method="POST">
-        </form>
-        <footer>
-        </footer>
-    </body>
-</html>
+<body>
+    <p>データベースへの接続に失敗しました。</p>
+    <a href="/">トップに戻る</a>
+</body>
