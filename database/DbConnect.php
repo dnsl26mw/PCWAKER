8bcc42d2917f55006cecf8beffa8ea8f856f5233
dnsl26mw <dnsl26mw@gmail.com>
2026-05-02 15:12:56 +0900

    Config.php、helper.phpファイル追加

--- database/DbConnect.php
+++ database/DbConnect.php
@@ -3,15 +3,20 @@
 require_once __DIR__ . '/../app/Http/Controllers/RoutesController.php';
 
 class DBConnect {
+
     // DBへの接続
     private static $pdo;
+
     public static function getDBConnect(){
 
         if(!self::$pdo){
+
             try{
+
                 // ('mysql: DB名前; host = IPまたはホスト名; charset = 文字コード', 'DBユーザ名', 'パスワード')
                 self::$pdo = new PDO('mysql:dbname=pcwaker;host=localhost;charset=utf8','pcwaker','pcwaker');
             } catch(PDOException $e) {
+                
                 // DB接続失敗
                 $routesController = new RoutesController();
                 $routesController->routesDisConnect();
