5c4bc7e0011f9c41b9a9af32bc9989c71340f4f0
dnsl26mw <dnsl26mw@gmail.com>
2026-07-30 12:14:19 +0900

    first commit

---
+++ database/migrations/2026_07_29_184811_create_devices_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->string('device_id')->nullable();
+            $table->string('device_name')->nullable();
+            $table->string('macaddress')->nullable();
+            $table->foreignId('user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
